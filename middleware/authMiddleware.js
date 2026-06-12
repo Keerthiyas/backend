0217197187487292426fe4e3695cf9cef8c92670
Keerthiyas <keerthiyakeerthiya360@gmail.com>
2026-06-12 22:09:32 +0530

    deploy fix 3

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,5 +1,5 @@
 const jsonwebtoken = require('jsonwebtoken');
-const User = require('../models/user');
+const User = require('../models/User');
 exports.authMiddleware = async(req,res,next)=>{
     try{
         const token = req.headers.authorization.split(" ")[1];
